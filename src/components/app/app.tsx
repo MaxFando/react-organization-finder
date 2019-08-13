439cf8cb7650ec4c51aafd7a0e30707a135f64bc
fandomj <fando97@mail.ru>
2019-08-13 20:48:22 +0300

    Добавляет структуру проекта

---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../header";
+
+import "./app.scss";
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <div className="header">
+      <h2>Header</h2>
+    </div>
+  );
+};
+
+export default Header;
